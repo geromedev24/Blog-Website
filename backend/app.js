1a7f6c966485a8ac587da74ab81983f64a78889c
geromedev24 <geromed1224@gmail.com>
2025-04-23 19:48:38 +0800

    Allow netlify app to send request to our render app.

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Whitelisting: CORS
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://codingcornergerome.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   exposedHeaders: ["Content-Type"],
